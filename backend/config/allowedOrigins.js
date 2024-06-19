70d5b3165d77a22dba79eb6840cfdcc0dc0ab274
AbdullahSeraj <106870849+AbdullahSeraj@users.noreply.github.com>
2024-06-19 04:24:04 -0700

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "https://full-stack-e-commerce-products.web.app"];
 
 module.exports = allowedOrigins;
