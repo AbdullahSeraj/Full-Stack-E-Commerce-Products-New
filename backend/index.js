685c08d2d355965410989cd4e4900e0b795c8a40
AbdullahSeraj <106870849+AbdullahSeraj@users.noreply.github.com>
2024-06-19 03:46:08 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser");
 
 app.use(
   cors({
-    origin: ["http://localhost:5000"],
+    origin: ["https://full-stack-e-commerce-products.web.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
